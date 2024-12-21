080d77b33a17abfb764293ea8144f031958291e8
Ashok Gopalakrishnan <workashok.g15@gmail.com>
2024-12-21 19:10:20 +0530

    arch_study

--- setup_study_all_develop.py
+++ setup_study_all_develop.py
@@ -5,11 +5,14 @@ import subprocess
 #This script will clone all the study repos and checkout develop, please use feature branches for new develeopments.
 
 subprocess.run(["git", "submodule", "add", "git@github.com:workashokg15/rust_study.git"])
+subprocess.run(["git", "submodule", "add", "git@github.com:workashokg15/arch_study.git"])
+
 
 subprocess.run(["git", "submodule", "update", "--init", "--recursive"])
 
 subprocess.run(["git", "submodule", "foreach", "git", "checkout", "-b", "develop"])
 
 
+subprocess.run()
 
 
